2f5d5db2d1c11d504319b528f0a71411b2fb5693
Mauro <mpino1701@gmail.com>
2023-01-03 18:16:31 -0300

    add control error conection refused an type use

--- network/procesingClient.py
+++ network/procesingClient.py
@@ -4,13 +4,20 @@ from core.tools import unpack, package
 class ProcesingClient:
     
     def __init__(self, **kargs):
-        self.socket = socket.socket()
-        self.socket.connect(("127.0.0.1", 7171))
+        self.ip = kargs.get("ip")
+        self.port = kargs.get("port")
+        self.socket = self.ConnectionServer()
         
-        test = {"Hola": "Adios"}
-        test = package(test)
-        self.socket.send(test)
-        packageReceive = unpack(self.socket.recv(1024))
-        print(packageReceive)
+    def ConnectionServer(self) -> socket:
+        try:
+            sock = socket.socket()
+            sock.connect(("127.0.0.1", 7171))
+            return sock
+        except TypeError as error:
+            print(error)
+            return None
+        except ConnectionRefusedError as error:
+            print(error)
+            return None
         
         
