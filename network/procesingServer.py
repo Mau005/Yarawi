ae762cd5d794d98fe8cfe96d1749e11129d02cc6
Mauro <mpino1701@gmail.com>
2023-01-05 14:35:43 -0300

    add status for procesing in server for client, control user active or not

--- network/procesingServer.py
+++ network/procesingServer.py
@@ -6,16 +6,43 @@ class ProcesingServer:
         self.client = kargs.get("client")
         self.addr = kargs.get("addr")
         self.motd = kargs.get("motd")
+        self.cifrated = 0
         self.status = True
-        
-    def send(self,pack):
+
+    @property
+    def cifrated(self):
+        return self._cifrated
+
+    @cifrated.getter
+    def cifrated(self):
+        return self._cifrated
+    @cifrated.setter
+    def cifrated(self, value):
+        self._cifrated = value
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
+
+    @status.getter
+    def status(self):
+        return self._status
+
+    def send(self, pack):
         self.client.send(package(pack))
-        
+
     def receive(self):
         return unpack(self.client.recv(1024))
 
     def run(self, *args):
         while self.status:
-            obj = self.receive()
-            if obj is not None:
-                print("Es informacion")
+            data = self.receive()
+            if data is not None:
+                self.procesing(data)
+
+    def procesing(self, data):
+        if data.get("status") == "init":
+            self.send({"status":True})
