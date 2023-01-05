1fac594415b5b031abea35270f4e196481235917
Mauro <mpino1701@gmail.com>
2023-01-05 14:35:59 -0300

    fix struct code

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
-import socket, sys
+import socket
+import sys
 import threading
 
 from network.procesingServer import ProcesingServer
 
 
 class Server:
-    
+
     def __init__(self, **kargs):
         self.ip = kargs.get("ip")
         self.port = kargs.get("port")
         self.motd = kargs.get("motd")
         self.socket = None
         self.listUser = []
-        
+
     def generate_socket(self) -> socket:
         try:
             sock = socket.socket()
@@ -22,23 +23,22 @@ class Server:
         except TypeError as error:
             input("Error IP or Port is None, press key to continue")
             sys.exit(1)
-        
+
     def run(self):
         self.socket = self.generate_socket()
         print("Servidor iniciado")
         while self.socket is not None:
             clt, addr = self.socket.accept()
-            obj = ProcesingServer(client = clt, addr = addr, motd = self.motd)
+            obj = ProcesingServer(client=clt, addr=addr, motd=self.motd)
             print(f"Se ha conectado: {addr}")
             self.listUser.append(obj)
-            th = threading.Thread(target = obj.run)
+            th = threading.Thread(target=obj.run)
             th.start()
-            
+
         self.socket.close()
         print("Servidor Cerrado")
-            
-        
-        
+
+
 if __name__ == "__main__":
     server = Server(ip="127.0.0.1", port=7171, motd="Bienvenidos a la app")
     server.run()
